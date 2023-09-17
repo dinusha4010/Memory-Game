162898dfe1009cf3fb11246bebab300af522ca22
KaluarachchiKCD <dinusha4010@gmail.com>
2023-09-17 11:12:53 +0300

    User Input added

--- src/memoryGame.java
+++ src/memoryGame.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 import java.util.Random;
 public class memoryGame {
 
@@ -5,8 +6,10 @@ public class memoryGame {
 		// TODO Auto-generated method stub
 		
 		Random random=new Random();
+		Scanner in= new Scanner(System.in);
 		
 		int[] numbers=new int[7];
+		int[] userInput=new int[7];
 		
 		for (int i=0;i<7;i++)
 			
@@ -36,7 +39,31 @@ public class memoryGame {
 		
 		
 		for (int i = 0; i < 20; ++i) System.out.println();
+		
+		for (int j=0; j<7;j++)
+			
+		{
+			System.out.println("Type number "+(j+1)+" :");
+			userInput[j]=Integer.parseInt(in.nextLine());
+		}
+		
+			System.out.println("The numbers were:");
+			for (int j=0;j<7;j++)
+			
+		{
+				System.out.print(numbers[j]);
+				
+		}
+			System.out.println();
+			System.out.println("Your numbers were:");
+			for (int j=0;j<7;j++)
+				
+			{
+					
+					System.out.print(userInput[j]);
+			}
 	}
+	
 
 	
 }
